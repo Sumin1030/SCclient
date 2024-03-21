3daace671de0ec45a904bf989fb759d1e59b188c
sumin <gjtnals258@gmail.com>
2024-03-21 01:06:28 -0400

    서버 주소 변경

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = (app) => {
   app.use(
     createProxyMiddleware('/api', {  //도메인 api로 호출
-      target: 'http://localhost:5001/', //통신할 서버의 도메인주소
+      target: 'http://ec2-18-217-16-111.us-east-2.compute.amazonaws.com:5001/', //통신할 서버의 도메인주소
       changeOrigin: true,
     })
   )
